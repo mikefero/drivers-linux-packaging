39dc5bc9cb87e221be9356ca1518f929e5301c3c
Michael Fero <michael.fero@datastax.com>
2016-06-20 11:17:54 -0400

    Initial automated package generation implementation for linux distros

---
+++ README.md
@@ -0,0 +1,65 @@
+# DataStax Drivers - Packaging for Linux Distros
+The [`build.sh`](https://github.com/mikefero/drivers-linux-packaging) script
+will perform the package building (e.g. release package generation) for the
+C/C++ DataStax drivers (core and DSE) against the following distros:
+
+- CentOS 5.11
+ - **NOTE:** DSE driver does not currently build for CentOS 5
+- CentOS 6.8
+- CentOS 7.2
+- Ubuntu 12.04 LTS
+- Ubuntu 14.04 LTS
+- Ubuntu 16.04 LTS
+
+## Obtaining Dependencies
+
+### Requirements
+- [Virtual Box](https://www.virtualbox.org/wiki/Downloads)
+- [Vagrant](https://www.vagrantup.com/downloads.html)
+
+## Usage - Building Packages for Distros
+
+```
+Usage: build.sh [OPTION...]
+
+    --clean                    enable packing directory clean
+    --help                     display this message
+
+Dependencies:
+    --libuv=(version)          libuv driver dependency version to build
+
+Drivers:
+    DataStax C/C++ Driver:
+      --cpp-core=(branch|tag)  DataStax C/C++ driver version to build
+      --cpp-dse=(branch|tag)   DataStax C/C++ DSE driver version to build
+        --two-factor           enable two-factor authentication [netrc]
+```
+
+### Example Build Configurations
+
+1. To build the packages with libuv v1.8.0 and DataStax C/C++ driver v2.4.1
+
+```
+./build.sh --libuv=1.8.0 --cpp-core=2.4.1
+```
+
+2. To build the packages with libuv v1.8.0, DataStax C/C++ driver v2.4.1, and
+   C/C++ DSE driver v1.0.0-eap1
+
+```
+./build.sh --libuv=1.8.0 --cpp-core=2.4.1 --cpp-dse=1.0.0-eap1
+```
+
+**NOTE:** DataStax C/C++ DSE driver is currently being developed in a private
+          repository. Access to the repository is required and can be supplied
+          using a private key `id_rsa`. If *two-factor* authentication is
+          enabled on your GitHub account, use `--two-factor` to enable
+          **Inetutils** libraries and ensure your credentials are supplied in
+          `netrc` file.
+
+# TODO
+- Update packages directory to mimic the downloads.datastax.com site
+- Simplify `Vagrantfile` and remove unnecessary configuration options
+- Vendor the libuv-packaging repository and combine into `build.sh` script
+- Remove multiple `Vagrantfile` scripts for heredoc generation in script
+- Add 32-bit packaging for applicable distros
---
+++ build.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+# Debug statements [if needed]
+#set -x #Trace
+#set -n #Check Syntax
+
+WORKING_DIRECTORY="$(pwd)"
+BASH_FILENAME="$(basename ${0})"
+SCRIPT_DIRECTORY=""$(dirname ${0})""
+
+# Constants
+PACKAGES_DIRECTORY="${SCRIPT_DIRECTORY}/packages"
+CPP_CORE_ARGUMENT="--cpp-core"
+CPP_DSE_ARGUMENT="--cpp-dse"
+LIBUV_ARGUMENT="--libuv"
+CLEAN_ARGUMENT="--clean"
+TWO_FACTOR_ARGUMENT="--two-factor"
+HELP_ARGUMENT="--help"
+
+##
+# Convert a passed in string to lowercase
+#
+# @param ${1} String to convert to lowercase
+# @return String converted to lowercase
+##
+to_lower() {
+  local string="${1}"
+  echo "${string}" | tr "[:upper:]" "[:lower:]"
+}
+
+##
+# Convert the time in seconds into a readable format
+#
+# @param ${1} Duration in seconds
+# @return Readable duration format
+##
+function to_duration {
+  # Calculate the values for the duration
+  local duration=${1}
+  local days=$((duration / 60 / 60 / 24))
+  local hours=$((duration / 60 / 60 % 24))
+  local minutes=$((duration / 60 % 60))
+  local seconds=$((duration % 60))
+
+  # Convert the duration (if values are valid)
+  local duration_format=
+  (( ${days} > 0 )) && duration_format="${duration_format}${days} days "
+  (( ${days} > 0 || ${hours} > 0 )) && duration_format="${duration_format}$[hours] hours "
+  (( ${days} > 0 || ${hours} > 0 || ${minutes} > 0 )) && duration_format="${duration_format}${minutes} minutes "
+  duration_format="${duration_format}${seconds} seconds"
+
+  # Return the readable format
+  echo "${duration_format}"
+}
+
+##
+# Create the packaging directory (if it does not exist)
+#
+# @param ${1} Directory to create in `package` directory
+##
+function create_packaging_directory {
+  local directory="${PACKAGES_DIRECTORY}/${1}"
+  if [ ! -d "${directory}" ]
+  then
+    mkdir -p ${directory}
+  fi
+}
+
+##
+# Print the usage of the script
+#
+# @param ${1} Exit code to use when exiting
+##
+print_usage() {
+  printf "Usage: `basename ${0}` [OPTION...]\n\n"
+  printf "    %-27s%s\n" "${CLEAN_ARGUMENT}" "enable packing directory clean"
+  printf "    %-27s%s\n" "${HELP_ARGUMENT}" "display this message"
+  printf "\nDependencies:\n"
+  printf "    %-27s%s\n" "${LIBUV_ARGUMENT}=(version)" "libuv driver dependency version to build"
+  printf "\nDrivers:\n"
+  printf "    DataStax C/C++ Driver:\n"
+  printf "      %-25s%s\n" "${CPP_CORE_ARGUMENT}=(branch|tag)" "DataStax C/C++ driver version to build"
+  printf "      %-25s%s\n" "${CPP_DSE_ARGUMENT}=(branch|tag)" "DataStax C/C++ DSE driver version to build"
+  printf "        %-23s%s\n" "${TWO_FACTOR_ARGUMENT}" "enable two-factor authentication [netrc]"
+  exit $1
+}
+
+# Parse the command line arguments
+CPP_CORE_VERSION=
+CPP_DSE_VERSION=
+LIBUV_VERSION=INVALID_VERSION
+CLEAN_ENABLED=false
+TWO_FACTOR_ENABLED=false
+ARGUMENT_COUNT=${#}
+if [ ${ARGUMENT_COUNT} -gt 0 ]
+then
+  # Iterate through each argument
+  for ((I = 0; I < ARGUMENT_COUNT; ++I))
+  do
+    # Ensure full command line argument are present
+    if [[ ${1} =~ .*=.* ]]
+    then
+      # Get the current argument and value
+      ARG=$(echo $(to_lower ${1}) | awk -F= '{print $1}')
+      VALUE=$(echo $(to_lower ${1}) | awk -F= '{print $2}')
+      shift
+
+      if [ "${ARG}" == "${CPP_CORE_ARGUMENT}" ]
+      then
+        CPP_CORE_VERSION=${VALUE}
+      elif [ "${ARG}" == "${CPP_DSE_ARGUMENT}" ]
+      then
+        CPP_DSE_VERSION=${VALUE}
+      elif [ "${ARG}" == "${LIBUV_ARGUMENT}" ]
+      then
+        LIBUV_VERSION=${VALUE}
+      fi
+    else
+      if [ "${1}" == "${CLEAN_ARGUMENT}" ]
+      then
+        CLEAN_ENABLED=true
+      elif [ "${1}" == "${HELP_ARGUMENT}" ]
+      then
+        print_usage 0
+      elif [ "${1}" == "${TWO_FACTOR_ARGUMENT}" ]
+      then
+        TWO_FACTOR_ENABLED=true
+      fi
+      
+      # Move to the next argument
+      shift
+    fi
+  done
+fi
+
+# Determine if the script should continue to process package build
+CONTINUE_PACKAGE_BUILD=true
+if [ "${LIBUV_VERSION}" == "INVALID_VERSION" ]
+then
+  printf "libuv version is required\n"
+  CONTINUE_PACKAGE_BUILD=false
+fi
+if [ -z "${CPP_CORE_VERSION}" ] && [ -z ${CPP_DSE_VERSION} ]
+then
+  printf "DataStax C/C++ driver [core|DSE] version is required\n"
+  CONTINUE_PACKAGE_BUILD=false
+fi
+if [ -n "${CPP_DSE_VERSION}" ] && [ ! -e "${SCRIPT_DIRECTORY}/id_rsa" ]
+then
+  printf "Private key is required: Ensure '${SCRIPT_DIRECTORY}/id_rsa' exists\n"
+  CONTINUE_PACKAGE_BUILD=false
+else
+  if [ "${TWO_FACTOR_ENABLED}" == "true" ] && [ ! -e "${SCRIPT_DIRECTORY}/netrc" ]
+  then
+    printf "Inetutils credentials is required: Ensure '${SCRIPT_DIRECTORY}/netrc' exists\n"
+    CONTINUE_PACKAGE_BUILD=false
+  fi
+fi
+if [ "${CONTINUE_PACKAGE_BUILD}" == "false" ]
+then
+  print_usage 1
+fi
+
+# Determine if the packaging directories should be cleaned
+if [ "${CLEAN_ENABLED}" == "true" ]
+then
+  printf "Cleaning packaging directory [${PACKAGES_DIRECTORY}] ..."
+  rm -rf ${PACKAGES_DIRECTORY}
+  printf " done.\n"
+fi
+
+#Get the starting time
+START_TIME=$(date +%s)
+
+#Perform the linux packaging
+declare -a FAILED_DISTROS
+VAGRANT_BOXES=( "vagrant/centos/5" "vagrant/centos/6" "vagrant/centos/7" "vagrant/ubuntu/12.04" "vagrant/ubuntu/14.04" "vagrant/ubuntu/16.04" )
+# Iterate through each vagrant box
+for vagrant_box in ${VAGRANT_BOXES[@]}
+do
+  # Create the packages directory (strip vagrant from path)
+  DISTRO=${vagrant_box#vagrant/}
+  create_packaging_directory ${DISTRO}
+
+  # Build the packing for the vagrant box
+  pushd ${SCRIPT_DIRECTORY}/${vagrant_box} > /dev/null 2>&1
+  CORE=${CPP_CORE_VERSION} DSE=${CPP_DSE_VERSION} LIBUV=${LIBUV_VERSION} TWO_FACTOR=${TWO_FACTOR_ENABLED} vagrant box update
+  CORE=${CPP_CORE_VERSION} DSE=${CPP_DSE_VERSION} LIBUV=${LIBUV_VERSION} TWO_FACTOR=${TWO_FACTOR_ENABLED} vagrant up
+  PACKAGING_ERROR_CODE=${?}
+  CORE=${CPP_CORE_VERSION} DSE=${CPP_DSE_VERSION} LIBUV=${LIBUV_VERSION} TWO_FACTOR=${TWO_FACTOR_ENABLED} vagrant destroy -f
+  popd > /dev/null 2>&1
+
+  # Determine if there was an issue creating the packages
+  if [ ${PACKAGING_ERROR_CODE} -ne 0 ]
+  then
+    FAILED_INDEX=${FAILED_DISTROS[@]}
+    FAILED_DISTROS[${FAILED_INDEX}]=${DISTRO}
+  fi
+done
+
+#Calculate the build time
+END_TIME=$(date +%s)
+PACKAGING_TIME=$((${END_TIME} - ${START_TIME}))
+DURATION=$(to_duration ${PACKAGING_TIME})
+printf "\nPackaging took ${DURATION}\n"
+
+# Determine if the packaging was successful or not
+if [ -z ${FAILED_DISTROS} ]
+then
+  printf "\n\nAll packages were created successfully\n"
+else
+  printf "\n\n### Failed to Create All Packages ###\n"
+  # Iterate through each failed distro
+  for failed_distro in ${FAILED_DISTROS[@]}
+  do
+    printf "  ${failed_distro}\n"
+  done
+  printf "### Failed to Create All Packages ###\n"
+  exit 2
+fi
